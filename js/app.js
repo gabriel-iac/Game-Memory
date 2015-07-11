3370d4a609a263f051768ddf8fdf8ece23b1098c
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-07-11 15:31:12 +0100

    it was working, now mysteriously turns doesen't alternate

--- js/app.js
+++ js/app.js
@@ -2,6 +2,7 @@ var cards = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
 var player1 = [];
 var player2= [];
 var turn = true;
+var playerTurn = $('.playerTurn');
 var gameStart = function(){
 	function shuffle(hand) {
 		for (var i=hand.length-1; i>0; i--) {
@@ -20,31 +21,46 @@ var gameStart = function(){
 	}
 
 	$('#board').html(items);
-$('.card').on('click', turnCard);
+	$('.card').on('click', turnCard);
 	console.log(shuffle(cards));
 }
 
 function turnCard(){
 	$(this).addClass('active');
-	if($('.active').length === 2){
+	if($('.active').length %2 === 0){
 		getPair();
 	}
 	else{
 		alert("turn another card")
-	}
+			}
 }
 
 function getPair(){
 	if($('.active:eq(0)').attr('data-card') === $('.active:eq(1)').attr('data-card')){
-alert("Match found")
+		alert("Match found")
+		if (turn == true){
+			player1.length++;
+			$('P1score').html('player 1' + " " + player1.length)
+		}
+		else
+		{
+			player2.length++;
+			$('.P2score').html('player 2' + " " +player2.length)
+		}
+	}
+	else{
+		alert('match not found')
+		if(turn == true){
+			$('.card').removeClass('active')
+			playerTurn.html('Player 1 Turn')
+			turn=false;
+		}
+		else{
+			playerTurn.html('Player 2 Turn')
+		$('.card').removeClass('active')
+		turn = true;
 }
-else{
-
-	$('.card').removeClass('active')
-
-alert('no match found')
-
-}
+	}
 }
 $(document).ready(function(){
 	$('#newGame').on('click', function(){
