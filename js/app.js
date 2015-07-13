0ef874d19b29ba69428e589340b825c6c6892191
gabriel-iac <gabriele.iacopetti@gmail.com>
2015-07-13 13:36:08 +0100

    now everything seems to work

--- js/app.js
+++ js/app.js
@@ -2,86 +2,79 @@ var cards = [1,2,3,4,5,6,7,8,9,10,11,12,1,2,3,4,5,6,7,8,9,10,11,12];
 var player1 = [];
 var player2= [];
 var turn = true;
-var playerTurn = $('#display');
+var display;
+
+function shuffle(hand) {
+	for (var i=hand.length-1; i>0; i--) {
+		var j = Math.floor(Math.random()*(i + 1));
+		var t = hand[i];
+		hand[i] = hand[j];
+		hand[j] = t;
+	}
+	return hand;
+}
+
 //function to shuffle cards and create the board
 function gameStart(){
-	function shuffle(hand) {
-		for (var i=hand.length-1; i>0; i--) {
-			var j = Math.floor(Math.random()*(i + 1));
-			var t = hand[i];
-			hand[i] = hand[j];
-			hand[j] = t;
-		}
+	display = $('#display');
+	display.html('Player 1 Turn');
 
-		return hand;
-	}
 	var board = shuffle(cards);
 	var items = "";
 	for(var i = 0; i<board.length; i++){
-		items +=  '<li class="flip-container" data-card="card-'+ board[i] +'">' + '<div class="flipper">' + '<div class="front">' + board[i] +'</div>'+ 
+		items +=  '<li class="flip-container" data-card="card-'+ board[i] +'" >' + '<div class="flipper">' + '<div class="front">' + board[i] +'</div>'+ 
 		'<div class="card card-'+ board[i] +'" >' + '</div>' +'</div>'+ '</li>';
 	}
 
 	$('#board').html(items);
-	$('li.flip-container').on('click',function(){
-$('.flipper .front').removeClass('front');
-	}, turnCard);
-	console.log(shuffle(cards));
-	player1=[0];
-	player2=[0];
+	$('li.flip-container').on('click', turnCard);
+
+	player1=[];
+	player2=[];
 }
 //function to turn cards during turn or return the winner.
 function turnCard(){
+	// The class of active is used to compare the two cards...
+	$(this).addClass('active flipped');
 
-	$(this).addClass('active');
 	if($('.active').length %2 === 0){
 		getPair();
-	}
-	else if(player1.length + player2.length === 24){
+	} else if(player1.length + player2.length === 24){
 		getWinner();
-	}
-	else{
-		alert("turn another card")
+	} else{
+		// alert("turn another card")
 	}
 }
 
 function getPair(){
 	if($('.active:eq(0)').attr('data-card') == $('.active:eq(1)').attr('data-card')){
 		alert("Match found")
-		if (turn == true){
+		if (turn == true) {
 			player1.length++;
 			$('.p1score').html('player 1' + " " +player1.length);
-			$('.active').addClass('p1-pair').off('click');
-			$('.card').removeClass('active');
-
-		}
-		else
-		{
+			// $('.active').addClass('p1-pair').off('click');
+		} else {
 			player2.length++;
 			$('.p2score').html('player 2' + " " +player2.length);
-			$('.active').addClass('p2-pair').off('click');
-			$('.flip-container').removeClass('active');
+			// $('.active').addClass('p2-pair').off('click');
 		}
-	}
-	else{
+	} else{
 		alert('match not found')
-		if(turn == true){
-			$('.flip-container').removeClass('active');
-			$('#display').html('Player 1 Turn');
-			turn=false;
-		}
-		else{
-			$('#display').html('Player 2 Turn');
-			$('.flip-container').removeClass('active');
+		$('.active').removeClass('flipped');
+		if(turn) {
+			display.html('Player 2 Turn');
+			turn = false;
+		} else {
+			display.html('Player 1 Turn');
 			turn = true;
 		}
 	}
+	$('.flip-container').removeClass('active');
 }
 //compare the arrays of the player to see who win
 function getWinner(){
 	if (player1.length > player2.length){
 		$('#display').html("player1 Wins")
-
 	}
 	else if(player1.length == player2.length){
 		$('#display').html("it's a tie");
@@ -90,14 +83,7 @@ function getWinner(){
 		$('#display').html("player 2 wins");
 	}
 }
-//
+
 $(document).ready(function(){
-	var playerTurn = $('#display');
-	$('#newGame').on('click', function(){
-		playerTurn.html('Player 1 Turn');
-
-		gameStart()
-		turn=true;
-
-	})
+	$('#newGame').on('click', gameStart)
 });
